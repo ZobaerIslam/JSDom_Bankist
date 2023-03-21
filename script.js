402d490eb3834e6cf740f5a04d36afadfc714f55
zobaer <zobaerislam62@gmail.com>
2023-03-21 17:43:34 +0600

    DOM manipulate Styles, Attributes and Classes

--- script.js
+++ script.js
@@ -75,3 +75,63 @@ document.querySelector('.btn--close-cookie').addEventListener('click', function(
   // message.parentElement.removeChild(message);
   message.remove();
 })
+
+
+// ** Styles **
+message.style.backgroundColor = '#37383d';
+message.style.width = '90%';
+message.style.margin = 'auto';
+
+console.log(message.style.color);
+console.log(message.style.backgroundColor);
+
+console.log(getComputedStyle(message).color);
+console.log(getComputedStyle(message).height);
+
+message.style.height = Number.parseFloat(getComputedStyle(message).height, 10) + 30 + 'px';
+console.log(getComputedStyle(message).height);
+
+
+document.documentElement.style.setProperty('--color-primary', 'orangered');
+
+
+// Attributes
+const logo = document.querySelector('.nav__logo');
+console.log(logo);
+console.log(logo.alt);
+console.log(logo.src);
+console.log(logo.className);
+
+logo.alt = 'Beautiful minimalist logo';
+console.log(logo.alt);
+
+// Non-standard 
+console.log(logo.designer);
+console.log(logo.getAttribute('designer'));
+logo.setAttribute('compamy', 'Banklist');
+
+console.log(logo.src);  // Absolute logo src
+// Relative logo src
+console.log(logo.getAttribute('src'));  
+
+const link = document.querySelector('.twitter-link');
+// both are Absolute link
+console.log(link.href);
+console.log(link.getAttribute('href'));
+
+const nav_link = document.querySelector('.nav__link--btn');
+console.log('Absolute link:', nav_link.href);   // Absolute nav link
+console.log('Relative link:', nav_link.getAttribute('href'));   // Relative nav link
+
+
+// Data attribute
+console.log(logo.dataset.versionNumber);
+
+// Classes
+logo.classList.add('c', 'j');
+logo.classList.remove('c', 'j');
+logo.classList.toggle('c');
+logo.classList.contains('c');  // not includes
+
+// Don't use  # overwrite all classes into this class name
+// logo.className = 'siam';
